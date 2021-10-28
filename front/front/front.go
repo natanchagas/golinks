49f5d94da9da6d95354b4f6e191a274b9fb41327
natanchagas <natan.c.silva@uol.com.br>
2021-10-27 22:22:49 -0300

    style: :lipstick: golinks, health pages
    golinks and header templates

--- front/front/front.go
+++ front/front/front.go
@@ -11,11 +11,19 @@ func Hello(c echo.Context) error {
 	return c.Render(http.StatusOK, "home.html", nil)
 }
 
+func ListGolinks(c echo.Context) error {
+	return c.Render(http.StatusOK, "golinks.html", nil)
+}
+
+func HealthCheck(c echo.Context) error {
+	return c.Render(http.StatusOK, "health.html", nil)
+}
+
 func Endpoints(e *echo.Echo) {
 	e.Static("/static", "static")
 	e.GET("/", Hello)
-	// e.GET("/health", HealthCheck)
 	// e.GET("/golink", CreateGolink)
-	// e.GET("/golinks", ListGolinks)
+	e.GET("/golinks", ListGolinks)
+	e.GET("/health", HealthCheck)
 	// e.GET("/:link", Golink)
 }
