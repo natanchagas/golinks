4bff6366f06ffd862e0336eb8ba9c155913eb696
natanchagas <natan.c.silva@uol.com.br>
2021-11-12 12:53:25 -0300

    feat: :test_tube: api endpoint from variable
    api endpoint from variable

--- front/front/front.go
+++ front/front/front.go
@@ -1,22 +1,37 @@
 package front
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
+func MakeData(extra map[string]interface{}) map[string]interface{} {
+	data := make(map[string]interface{})
+	data["API_ENDPOINT"] = os.Getenv("API_ENDPOINT")
+
+	for key, value := range extra {
+		fmt.Println(key, value)
+		data[key] = value
+	}
+
+	return data
+}
+
 func Hello(c echo.Context) error {
 	// return c.Render(http.StatusOK, "hello", template.HTML("<p>HTML Test</p>"))
-	return c.Render(http.StatusOK, "home.html", nil)
+	fmt.Println(MakeData(nil))
+	return c.Render(http.StatusOK, "home.html", MakeData(nil))
 }
 
 func ListGolinks(c echo.Context) error {
-	return c.Render(http.StatusOK, "golinks.html", nil)
+	return c.Render(http.StatusOK, "golinks.html", MakeData(nil))
 }
 
 func HealthCheck(c echo.Context) error {
-	return c.Render(http.StatusOK, "health.html", nil)
+	return c.Render(http.StatusOK, "health.html", MakeData(nil))
 }
 
 func Endpoints(e *echo.Echo) {
