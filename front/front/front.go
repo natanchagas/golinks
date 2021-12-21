d1feb2f87e52be8f15398708afd652c793251ecd
natanchagas <natan.c.silva@uol.com.br>
2021-12-21 08:30:45 -0300

    feat: :sparkles: create link and error
    add create link func, error page

--- front/front/front.go
+++ front/front/front.go
@@ -1,6 +1,7 @@
 package front
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
@@ -28,16 +29,46 @@ func Hello(c echo.Context) error {
 	return c.Render(http.StatusOK, "home.html", MakeData(nil))
 }
 
+func CreateGolink(c echo.Context) error {
+
+	link := c.FormValue("url")
+
+	var g objects.Golink
+	g.OriginalUrl = link
+
+	json_data, json_err := json.Marshal(g)
+	if json_err != nil {
+		log.Println(json_err)
+		return c.Render(http.StatusOK, "error.html", MakeData(nil))
+	}
+
+	resp, get_err := http.Post(os.Getenv("API_ENDPOINT")+"/golink", "application/json", bytes.NewBuffer(json_data))
+	if get_err != nil {
+		log.Println(get_err)
+		return c.Render(http.StatusOK, "error.html", MakeData(nil))
+	}
+
+	var res objects.Golink
+	json.NewDecoder(resp.Body).Decode(&res)
+
+	data := make(map[string]interface{})
+	data["golink"] = res
+
+	return c.Render(http.StatusOK, "golink.html", MakeData(data))
+}
+
 func ListGolinks(c echo.Context) error {
 
 	resp, get_err := http.Get(os.Getenv("API_ENDPOINT") + "/golinks")
 	if get_err != nil {
 		fmt.Println(get_err)
+		return c.Render(http.StatusOK, "error.html", MakeData(nil))
 	}
 
 	body, read_err := ioutil.ReadAll(resp.Body)
 	if read_err != nil {
 		log.Fatalln(read_err)
+		return c.Render(http.StatusOK, "error.html", MakeData(nil))
 	}
 
 	var links []objects.Golink
@@ -47,8 +78,6 @@ func ListGolinks(c echo.Context) error {
 	data := make(map[string]interface{})
 	data["golinks"] = links
 
-	log.Println(MakeData(data))
-
 	return c.Render(http.StatusOK, "golinks.html", MakeData(data))
 }
 
@@ -57,11 +86,13 @@ func HealthCheck(c echo.Context) error {
 	resp, get_err := http.Get(os.Getenv("API_ENDPOINT") + "/health")
 	if get_err != nil {
 		fmt.Println(get_err)
+		return c.Render(http.StatusOK, "error.html", MakeData(nil))
 	}
 
 	body, read_err := ioutil.ReadAll(resp.Body)
 	if read_err != nil {
 		log.Fatalln(read_err)
+		return c.Render(http.StatusOK, "error.html", MakeData(nil))
 	}
 
 	var health objects.Health
@@ -71,16 +102,23 @@ func HealthCheck(c echo.Context) error {
 	data := make(map[string]interface{})
 	data["health"] = health
 
-	log.Println(MakeData(data))
-
 	return c.Render(http.StatusOK, "health.html", MakeData(data))
+}
+
+func Golink(c echo.Context) error {
+	return c.Redirect(http.StatusMovedPermanently, os.Getenv("API_ENDPOINT")+"/"+c.Param("link"))
+}
+
+func Error(c echo.Context) error {
+	return c.Render(http.StatusOK, "error.html", MakeData(nil))
 }
 
 func Endpoints(e *echo.Echo) {
 	e.Static("/static", "static")
 	e.GET("/", Hello)
-	// e.GET("/golink", CreateGolink)
+	e.POST("/golink", CreateGolink)
 	e.GET("/golinks", ListGolinks)
 	e.GET("/health", HealthCheck)
-	// e.GET("/:link", Golink)
+	e.GET("/:link", Golink)
+	e.GET("/error", Error)
 }
